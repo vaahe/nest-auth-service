58dc61fa2e9e0233a87acd2efb56680aec178de1
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2025-01-31 19:18:31 +0400

    import required modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,29 @@
-import { Module } from "@nestjs/common";
-import { AuthController } from "./auth.controller";
-import { AuthService } from "./auth.service";
-import { UserModule } from "src/user/user.module";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { User } from "src/user/entities/user.entity";
 import { JwtModule } from '@nestjs/jwt'
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserModule } from "src/user/user.module";
+import { User } from "src/user/entities/user.entity";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { JwtAuthGuard } from "./guards/jwt-auth.guard";
+import { JwtStrategy } from "./strategies/jwt.strategy";
 
 @Module({
     imports: [
         UserModule,
         TypeOrmModule.forFeature([User]),
-        JwtModule.register({ secret: 'fd', signOptions: { expiresIn: '1d' } })
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                secret: configService.get('JWT_SECRET'),
+            })
+        })
     ],
     controllers: [AuthController],
-    providers: [AuthService],
+    providers: [AuthService, JwtAuthGuard, JwtStrategy],
     exports: [AuthService]
 })
 export class AuthModule { }
