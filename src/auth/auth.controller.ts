a4d8b928ba7ac1ef1dd6c8275f7d657a67830c83
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2025-01-31 19:18:18 +0400

    create auth routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,29 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { User } from 'src/user/entities/user.entity';
+import { AuthenticatedRequest, GeneratedTokens } from 'src/types';
+import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
+
 import { AuthService } from './auth.service';
-import { SigninDto } from './dto/signin.dto';
-import { SignupDto } from './dto/signup.dto';
+import { SignInDto } from './dto/sign-in.dto';
+import { SignUpDto } from './dto/sign-up.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
     @Post('signin')
-    signin(@Body() signinDto: SigninDto) {
-        const isValid = this.authService.validateUser(signinDto.username, signinDto.password);
+    signin(@Body() signInDto: SignInDto): Promise<GeneratedTokens> {
+        return this.authService.signin(signInDto);
+    }
 
-        if (isValid) {
-            return { message: "Login successful" };
-        } else {
-            return { message: "Login failed" };
-        }
+    @Post('signup')
+    signup(@Body() signUpDto: SignUpDto): Promise<GeneratedTokens> {
+        return this.authService.signup(signUpDto);
+    }
+
+    @Get('me')
+    @UseGuards(JwtAuthGuard)
+    async getMe(@Req() req: AuthenticatedRequest): Promise<User> {
+        return req.user;
     }
 }
