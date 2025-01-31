635f5778e2d17a6070fabf174430ef55deb62945
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2025-01-31 19:19:02 +0400

    implement auth business logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,55 @@
-import { Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { BadRequestException, ConflictException, Injectable } from "@nestjs/common";
+
+import { GeneratedTokens } from "src/types";
+import { UserService } from "src/user/user.service";
+import { User } from "src/user/entities/user.entity";
+
+import { JwtUtils } from "src/utils/jwt.util";
+import { PasswordUtils } from 'src/utils/password.util';
+
+import { SignInDto } from "./dto/sign-in.dto";
+import { SignUpDto } from "./dto/sign-up.dto";
+
 
 @Injectable()
 export class AuthService {
-    async validateUser(username: string, password: string): Promise<any> {
-        if (username === 'admin' && password === 'admin') {
-            return { username: 'admin' };
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService
+    ) { }
+
+    async signin(signInDto: SignInDto): Promise<GeneratedTokens> {
+        const { username, password } = signInDto;
+
+        const user = await this.userService.getUserByUsername(username);
+        if (!user) {
+            throw new BadRequestException('Invalid credentials');
         }
 
-        return null;
+        const isPasswordValid = await PasswordUtils.comparePasswords(password, user.password);
+        if (!isPasswordValid) {
+            throw new BadRequestException('Invalid credentials');
+        }
+
+        return JwtUtils.generateTokens(user.id, this.jwtService);
     }
 
-    async signin(username: string, password: string): Promise<any> {
-        return this.validateUser(username, password);
+    async signup(signUpDto: SignUpDto): Promise<GeneratedTokens> {
+        const { username, password, role } = signUpDto;
+
+        const existingUser = await this.userService.getUserByUsername(username);
+        if (existingUser) {
+            throw new ConflictException('User already exists');
+        }
+
+        const hashedPassword = await PasswordUtils.hashPassword(password);
+        const createdUser = await this.userService.createUser({
+            username,
+            password: hashedPassword,
+            role,
+        } as User);
+
+        return JwtUtils.generateTokens(createdUser.id, this.jwtService);
     }
 }
