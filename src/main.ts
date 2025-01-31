03bbf9d91e1d58df6ed49872a4467c6a2f0da83e
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2025-01-31 19:20:16 +0400

    remove hard codes

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app/app.module';
+import { appConfig } from './config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+  await app.listen(appConfig.port);
 }
 
 bootstrap();
